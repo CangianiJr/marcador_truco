3974f214f084ecfc46ffda62a0902a0d30bb6df1
Everson Cangiani Jr <juniorcangiani01@gmail.com>
2022-09-22 21:08:08 -0300

    Página marcador pronta

--- lib/app/pages/resultado_page.dart
+++ lib/app/pages/resultado_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:marcador_truco/app/pages/marcador_page.dart';
-import 'package:marcador_truco/app/rotinas/pontuacao_rotina.dart';
 
 class ResultadoPage extends StatelessWidget {
   const ResultadoPage({Key? key, required this.vencedores}) : super(key: key);
@@ -20,7 +19,6 @@ class ResultadoPage extends StatelessWidget {
           Text(vencedores),
           TextButton(
             onPressed: () {
-              PontuacaoRotina().reiniciarPontuacao();
               Navigator.push(context, MaterialPageRoute(builder: (context) {
                 return const MarcadorPage();
               }));
