fb24b39dd379c72cd5639dc771f109a8cd656638
emannuelbrito <emannuelbrito2018@gmail.com>
2022-09-22 22:53:51 -0300

    Pagina de resultado

--- lib/app/pages/resultado_page.dart
+++ lib/app/pages/resultado_page.dart
@@ -13,21 +13,57 @@ class ResultadoPage extends StatelessWidget {
         title: const Text("Resultado"),
         centerTitle: true,
         automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
       ),
-      body: Column(
-        children: [
-          const Text("gay"),
-          Text(vencedores),
-          TextButton(
-            onPressed: () {
-              PontuacaoRotina().reiniciarPontuacao();
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const MarcadorPage();
-              }));
-            },
-            child: const Text("Reiniciar jogo"),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // ignore: avoid_unnecessary_containers
+              Container(
+                child: Column(children: [
+                  const Text(
+                    "Vencedor",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    vencedores,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 35),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset("assets/images/Trofeu.png", height: 400),
+                ]),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {
+                  PontuacaoRotina().reiniciarPontuacao();
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const MarcadorPage();
+                  }));
+                },
+                child: const Text(
+                  "Reiniciar",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
